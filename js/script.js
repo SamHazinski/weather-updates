3f3c710aaee9e06c7fa3b92c5b768db382002e1a
Sam Hazinski <shazinski@gmail.com>
2024-06-06 18:09:40 -0400

    displaying data on cards and shows the next 5 days

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const searchFormEl = document.querySelector('#search-form');
 const resultCont = document.querySelector('#result-content');
+const fivecast = document.querySelector('#five-cast');
 
 function handleSearchFormSubmit(event) {
   event.preventDefault();
@@ -31,14 +32,49 @@ fetch(apiUrl)
       return res.json();
     })
     .then(function (newData) {
-      for (i = 0; i<6; i++){
+      for (i = 0; i<60; i++){ 
        console.log(newData.list[i].main.temp);
        console.log(newData.list[i].main.humidity);
        console.log(newData.list[i].wind.speed); 
+       console.log(newData.list[i].dt_txt); 
+//8
+       if(i === 0){
+        const newDiv = document.createElement('div');
+        const newh3 = document.createElement('h3');
+        const newP = document.createElement('p');
+        const newP2 = document.createElement('p');
+        const newP3 = document.createElement('p');
+        newDiv.classList.add('card', 'mb-3', 'p-3');
+        newh3.textContent = `Today's weather in ${inputName} is`
+        newP.textContent = `Temp: ${newData.list[i].main.temp} degrees F`
+        newP2.textContent = `Wind: ${newData.list[i].wind.speed} Miles per hour`
+        newP3.textContent = `Humidity: ${newData.list[i].main.humidity}%`
+        resultCont.appendChild(newh3);
+        resultCont.appendChild(newDiv);
+        newDiv.appendChild(newP);
+        newDiv.appendChild(newP2);
+        newDiv.appendChild(newP3);
+       } else if (i === 7 ||i === 15 || i ===23 || i ===31 || i ===39 ||i ===47){
+       
+        const newDiv = document.createElement('div');
+        const newh3 = document.createElement('h3');
+        const newP = document.createElement('p');
+        const newP2 = document.createElement('p');
+        const newP3 = document.createElement('p');
+        newDiv.classList.add('card', 'mb-3', 'p-3');
+        newh3.textContent = `${newData.list[i].dt_txt} weather in ${inputName} is`
+        newP.textContent = `Temp: ${newData.list[i].main.temp} degrees F`
+        newP2.textContent = `Wind: ${newData.list[i].wind.speed} Miles per hour`
+        newP3.textContent = `Humidity: ${newData.list[i].main.humidity}%`
+        fivecast.appendChild(newh3);
+        fivecast.appendChild(newDiv);
+        newDiv.appendChild(newP);
+        newDiv.appendChild(newP2);
+        newDiv.appendChild(newP3);
 
-       const newDiv = document.createElement('div');
-       newDiv.textContent = `this is data for call ${i} ${newData.list[i].main.temp} ${newData.list[i].wind.speed} ${newData.list[i].main.humidity}`
-       resultCont.appendChild(newDiv);
+       }
+
+      
       }
 
       })
