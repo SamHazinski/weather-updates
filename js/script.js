138ac33963a459d0aa91292ac69f0d46c50c9051
Sam Hazinski <shazinski@gmail.com>
2024-06-06 16:04:01 -0400

    added first and second API calls

--- js/script.js
+++ js/script.js
@@ -10,10 +10,39 @@ const searchFormEl = document.querySelector('#search-form');
 function handleSearchFormSubmit(event) {
   event.preventDefault();
   // console.log("working");
-  const valInput = document.getElementById("search-input").value;
-  console.log(valInput);
-  // localStorage.setItem(valInput, JSON.stringify(valInput));
-
+  const cityName = document.getElementById("search-input").value;
+  console.log(cityName);
+  // localStorage.setItem(cityName, JSON.stringify(cityName));
+  callApi(cityName);
 }
+
+function callApi(inputName){
+  const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${inputName},&limit=1&APPID=3eff6f3173e6337e807c89458953d7b6`
+  
+fetch(apiUrl)
+.then(function (res) {
+  return res.json();
+})
+  .then(function (data) {
+    // console.log(data);
+    // console.log(data[0].lat)
+    // console.log(data[0].lon)
+    const lat = data[0].lat;
+    const lon = data[0].lon;
+    console.log(lat)
+    console.log(lon)
+    const apiTwo = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=3eff6f3173e6337e807c89458953d7b6&units=imperial`
+    fetch(apiTwo)
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function (data) {
+       console.log(data);
+      })
+})
+}
+
+
+
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
